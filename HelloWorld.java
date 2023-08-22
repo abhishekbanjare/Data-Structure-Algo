ef4f90be2b23b17100e23fb743ee4c76e7b63fb5
Abhishek <suryaabhishek329@gmail.com>
2023-08-22 15:31:26 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class HelloWorld{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        System.out.println("This is my first java program");
+
+        int a = 1000;
+        int b = 2000;
+        int sum = a + b;
+        System.out.println("a + b = "+sum);
+    }
+}
